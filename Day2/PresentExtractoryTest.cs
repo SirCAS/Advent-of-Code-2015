1b577bed42c73970c58d858882b1117906ee350c
Christoffer Illum Søndergaard <sircas@sircas.dk>
2015-12-05 02:06:24 +0100

    Improved test coverage

---
+++ Day2/PresentExtractoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day2
+{
+    [TestFixture]
+    public class PresentExtractorTest
+    {
+        [Test]
+        public void ExtractThreePresentsTest()
+        {
+            // 1. Arrange
+            var data = "29x13x26 \n 11x11x14 \r\n 27x2x5";
+
+            IList<PresentModel> result = null;
+
+            // 2. Act + 3. Assert
+            Assert.DoesNotThrow( delegate { result = PresentExtractor.ExtractPresents(data); });
+            Assert.AreEqual(3, result.Count);
+        }
+    }
+}
