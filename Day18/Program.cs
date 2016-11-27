151a7a40d3e4171a990055197fa238ffc773374b
Christoffer Illum Søndergaard <sircas@sircas.dk>
2016-11-27 12:20:51 +0100

    Added solution for day 18 part 1

---
+++ Day18/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day18.ConsoleApplication
+{
+    public static class ExtensionMethods
+    {
+        public static void Print(this bool[][] grid)
+        {
+            foreach(var line in grid)
+            {
+                foreach(var light in line)
+                {
+                    Console.Write(light ? '#' : '.');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Tick(this bool[][] grid, int ticks)
+        {
+            for(int x = 0; x<ticks; ++x)
+            {
+                Tick(grid);
+            }
+        }
+
+        public static int TurnedOnLights(this bool[][] grid)
+        {
+            return grid.SelectMany(x => x).Count(x => x);
+        }
+
+        public static void Tick(this bool[][] grid)
+        {
+            // All of the lights update simultaneously; they all consider the same current state before moving to the next.
+            var newGrid = new bool[grid.Length][];
+            for(int x=0; x<grid.Length; ++x)
+                newGrid[x] = new bool[grid.Length];
+
+            for(int x=0; x<grid.Length; ++x)
+            {
+                for(int y=0; y<grid[x].Length; ++y)
+                {
+                    var isOn = grid[x][y];
+                    var neighbors = GetNeighbors(grid, x, y);
+                    
+                    if(isOn) // A light which is on stays on when 2 or 3 neighbors are on, and turns off otherwise.
+                    {                    
+                        if(neighbors == 2 || neighbors == 3)
+                        {
+                            newGrid[x][y] = true;
+                        } else {
+                            newGrid[x][y] = false;
+                        }
+                    }
+                    else
+                    { // A light which is off turns on if exactly 3 neighbors are on, and stays off otherwise.
+                        if(neighbors == 3)
+                        {
+                            newGrid[x][y] = true;
+                        } else {
+                            newGrid[x][y] = false;
+                        }
+                    }
+                }
+            }
+
+            for(int x=0; x<grid.Length; ++x)
+                grid[x] = newGrid[x];
+        }
+
+        private static int GetNeighbors(bool[][] grid, int x, int y)
+        {
+            int neighbors = 0;
+            int s = grid.Length - 1;
+            //      x           
+            //  +--------
+            //  | 1  2  3
+            // y| 4  X  5
+            //  | 6  7  8 
+            //
+            if(x > 0 && y > 0 && grid[x - 1][y - 1]) ++neighbors; // 1
+            if(y > 0 &&          grid[  x  ][y - 1]) ++neighbors; // 2
+            if(x < s && y > 0 && grid[x + 1][y - 1]) ++neighbors; // 3
+            if(x > 0 &&          grid[x - 1][  y  ]) ++neighbors; // 4
+            if(x < s &&          grid[x + 1][  y  ]) ++neighbors; // 5
+            if(x > 0 && y < s && grid[x - 1][y + 1]) ++neighbors; // 6
+            if(y < s &&          grid[  x  ][y + 1]) ++neighbors; // 7
+            if(x < s && y < s && grid[x + 1][y + 1]) ++neighbors; // 8
+            return neighbors;
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------------+");
+            Console.WriteLine("| Advent of Code - Day 18 |");
+            Console.WriteLine("+-------------------------+");
+
+            // const int steps = 4;
+            // const string DataFileName = "input.1.txt";
+            const int steps = 100;
+            const string DataFileName = "input.txt";
+
+            // Load data from input file
+            var grid = File.ReadAllLines(DataFileName)
+                .Select(
+                    x => x.Select(y => y == '#' ? true : false)
+                          .ToArray())
+                .ToArray();
+        
+            grid.Tick(steps);
+
+            grid.Print();
+
+            var turnedOnLights = grid.TurnedOnLights();
+
+            Console.WriteLine($"There is {turnedOnLights} after {steps}");
+
+            Console.WriteLine($"  -Gruss Gott");
+        }
+    }
+}
