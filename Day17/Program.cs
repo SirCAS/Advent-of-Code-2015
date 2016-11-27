de3720de46df42c7d3d6fbeb82832948b2955b05
Christoffer Illum Søndergaard <sircas@sircas.dk>
2016-11-27 10:15:21 +0100

    Added soultion for day 17 (part 1 and 2)

---
+++ Day17/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day17.ConsoleApplication
+{
+    public class Program
+    {
+        private const string DataFileName = "input.txt";
+
+        private const int Target = 150;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------------+");
+            Console.WriteLine("| Advent of Code - Day 17 |");
+            Console.WriteLine("+-------------------------+");
+
+            // Load data from input file
+            var containers = File.ReadAllLines(DataFileName)
+                .Select(int.Parse)
+                .ToList();
+
+            var solutions = GetSolutions(new List<int>(), containers, Target).ToList();
+
+            var totalSolutions = solutions.Count;
+            
+            var min = solutions.Min(p => p.Count);
+            var totalMinimumSolutions = solutions.Count(p => p.Count == min);
+
+            Console.WriteLine($"There is {totalSolutions} solutions in total and {totalMinimumSolutions} minimum solutions");
+
+            // Target = 25 l
+            // Containers = 1:20, 2:15, 3:10, 4:5, and 5:5
+            //
+            // 2:15, 3:10
+            // 1:20, 4:5
+            // 1:20, 5:5
+            // 2:15, 4:5, 5:5
+
+            Console.WriteLine($"  -Gruss Gott");
+        }
+
+        public static IEnumerable<List<int>> GetSolutions(List<int> solution, List<int> containers, int remaining)
+        {
+            for(int x = 0; x < containers.Count; ++x)
+            {
+                var container = containers[x];
+                if(container > remaining) // Skip if we have exceeded target
+                {
+                    continue;
+                }
+
+                // Create new solution set
+                var copy = solution.ToList();
+                copy.Add(container);
+
+                // Check if it is a match
+                if(container == remaining)
+                {
+                    yield return copy;
+                }
+                else
+                {
+                    // Keep iterating untill match or we exceed target
+                    foreach(var s in GetSolutions(copy, containers.Skip(x + 1).ToList(), Target - copy.Sum()))
+                    {
+                        yield return s;
+                    }
+                }
+           }
+        }
+    }
+}
