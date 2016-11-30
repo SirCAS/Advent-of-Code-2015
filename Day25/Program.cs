9c0e182de200e744e747c0e66f550fc334b1bf22
Christoffer Illum Søndergaard <sircas@sircas.dk>
2016-11-30 18:57:50 +0100

    Prepared new solution for day 25

---
+++ Day25/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day25.ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("+-------------------------+");
+            Console.WriteLine("| Advent of Code - Day 25 |");
+            Console.WriteLine("+-------------------------+");
+
+
+            Console.WriteLine($"  -Gruss Gott");
+        }
+    }
+}
